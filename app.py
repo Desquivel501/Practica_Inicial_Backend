0afa77b3055275df3f767323a3c49d8581b2916b
Jose2808 <3013297960101@ingenieria.usac.edu.gt>
2021-09-22 20:29:26 -0600

    Modificar datos y agregar curso

--- app.py
+++ app.py
@@ -3,26 +3,19 @@ from flask_cors import CORS
 import json
 import pyodbc
 
-# conn = pyodbc.connect('Driver={SQL Server};'
-#     'Server=DESKTOP-0DSCFJO\SQLEXPRESS;'
-#     'Database=anotherdatabase;'
-#     'Trusted_Connection=yes;')
-
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=DESKTOP-D3E52S7\SQLEXPRESS;'
-                      'Database=mydatabase;'
-                      'Trusted_Connection=yes;')
-
-
-
-cursor = conn.cursor()
-
-
-
 
 
 app = Flask(__name__)
 CORS(app)
+
+
+conn = pyodbc.connect('Driver={SQL Server};'
+'Server=DESKTOP-0DSCFJO\SQLEXPRESS;'
+'Database=mydatabase;''Trusted_Connection=yes;')
+
+
+cursor = conn.cursor()
+
 
 @app.route('/', methods = ['GET'])
 def frontpage():
@@ -31,20 +24,17 @@ def frontpage():
 @app.route('/login', methods = ['POST'])
 def login():
     if request.method == 'POST':
-        cursor.execute("SELECT * FROM Usuarios")
-        for usuario in cursor:
-            print(usuario)
-        user = request.json['user']
+        carnet = request.json['carnet']
         contraseña = request.json['password']
-        cursor.execute("SELECT * FROM Usuarios WHERE Usuario = " + f"'{user}'")
+        cursor.execute("SELECT * FROM Usuarios WHERE Carnet = " + f"'{carnet}';")
         for usuario in cursor:
-            if usuario[2] == user:
+            if usuario[2] == carnet:
                 if usuario[3] == contraseña:
                     objeto = {
                         'mensaje': 'Sesión iniciada con éxito',
                         'nombre': usuario[0],
                         'apellido': usuario[1],
-                        'usuario': usuario[2],
+                        'carnet': usuario[2],
                         'exist': True
                     }
                     return(jsonify(objeto))
@@ -73,16 +63,15 @@ def register():
         nombre = request.json['name']
         apellido = request.json['lastname']
         carnet = int(request.json['carnet'])
-        user = request.json['user']
+        correo = request.json['mail']
         contraseña = request.json['password']
         try:
-            cursor.execute(f"""INSERT INTO Usuarios (Nombre, Apellido, Usuario, Contraseña, Carné) values('{nombre}','{apellido}','{user}','{contraseña}', {carnet})""")
+            cursor.execute(f"INSERT INTO Usuarios (Nombre, Apellido, Carnet, Contraseña, Correo) values('{nombre}','{apellido}','{carnet}','{contraseña}', '{correo}');")
             conn.commit()
             objeto = {
                 'mensaje':'Usuario ingresado exitosamente',
                 'register': True
             }
-            
             return(jsonify(objeto))
         except:
             objeto = {
@@ -95,14 +84,14 @@ def register():
 @app.route('/forgot_password', methods = ['POST'])
 def forgot_password():
     if request.method == 'POST':
-        user = request.json['mail']
+        correo = request.json['mail']
         new_password = request.json['newpassword']
-        carnet = int(request.json['carnet'])
-        cursor.execute("SELECT * FROM Usuarios WHERE Usuario = " + f"'{user}'")
+        carnet = request.json['carnet']
+        cursor.execute("SELECT * FROM Usuarios WHERE Carnet = " + f"'{carnet}'")
         for usuario in cursor:
-            if usuario[2] == user:
-                if usuario[4] == carnet:
-                    cursor.execute("UPDATE Usuarios set Contraseña = " + f"'{new_password}'" + "WHERE Usuario = " + f"'{user}'")
+            if usuario[2] == carnet:
+                if usuario[4] == correo:
+                    cursor.execute("UPDATE Usuarios set Contraseña = " + f"'{new_password}'" + "WHERE Carnet = " + f"'{carnet}';")
                     conn.commit()
                     objeto = {
                         'mensaje': 'La contraseña se ha modificado exitosamente',
@@ -111,25 +100,22 @@ def forgot_password():
                     return(jsonify(objeto))
                 else:
                     objeto = {
-                        'mensaje': 'El número de carnet es incorrecto',
+                        'mensaje': 'El correo que ha ingresado es incorrecto',
                         'modified': False
                     }
                     return(jsonify(objeto))
             else:
                 objeto = {
-                    'mensaje':'El nombre de usuario no se ha encontrado o no existe',
+                    'mensaje':'El nombre de carnet que ha ingresado es incorrecto o no existe',
                     'modified': False
                 }
                 return(jsonify(objeto))
         objeto = {
-            'mensaje': 'El correo electrónico es incorrecto o no existe',
+            'mensaje': 'El registro académico ingresado es incorrecto o no existe',
             'modified': False
         }
         return(jsonify(objeto))
 
-
-                
-    pass
 
 @app.route('/catedraticos', methods = ['GET'])
 def getCatedraticos():
@@ -147,7 +133,7 @@ def getCatedraticos():
 @app.route('/cursos', methods = ['GET'])
 def getCursos():
 
-    cursor.execute('SELECT * FROM mydatabase.dbo.Cursos')
+    cursor.execute('SELECT * FROM Cursos;')
     Datos=[]
     for row in cursor:
         objeto = {
@@ -168,7 +154,7 @@ def crearPublicacion():
     
     if tipo == "curso":
         curso = request.json['curso']
-        sql = "INSERT INTO Publicaciones (NombreUsuario, CarnetUsuario, NombreCurso, NombreCatedratico, Titulo, Contenido) VALUES (?, ?, ?, ?, ?, ?)"
+        sql = "INSERT INTO Publicaciones (NombreUsuario, CarnetUsuario, NombreCurso, NombreCatedratico, Titulo, Contenido) VALUES (?, ?, ?, ?, ?, ?);"
         val = (nombre, carnet, curso, None, titulo, contenido)
         cursor.execute(sql,val)
         conn.commit()
@@ -176,7 +162,7 @@ def crearPublicacion():
         
     elif tipo == "catedratico":
         catedratico = request.json['catedratico']
-        sql = "INSERT INTO Publicaciones (NombreUsuario, CarnetUsuario, NombreCurso, NombreCatedratico, Titulo, Contenido) VALUES (?, ?, ?, ?, ?, ?)"
+        sql = "INSERT INTO Publicaciones (NombreUsuario, CarnetUsuario, NombreCurso, NombreCatedratico, Titulo, Contenido) VALUES (?, ?, ?, ?, ?, ?);"
         val = (nombre, carnet, None, catedratico, titulo, contenido)
         
         cursor.execute(sql,val)
@@ -205,9 +191,81 @@ def getPublicacion():
         Datos.append(objeto)
     return jsonify(Datos)
 
-    
+@app.route('/agregar_curso', methods = ['POST'])
+def agregar_curso():
+    carnet = request.json['carnet']
+    curso = request.json['curso']
+    try:
+        cursor.execute(f"INSERT INTO cursosAprobados (Carnet, Curso) VALUES('{carnet}', '{curso}');")
+        conn.commit()
+        objeto = {
+            'mensaje':'Curso agregado exitosamente'
+        }
+    except:
+        objeto = {
+            'mensaje':'Ocurrió un error al momento de agregar el curso, por favor intente de nuevo'
+        }
+    return(jsonify(objeto))
 
-        
+@app.route('/obtener_cursos', methods = ['POST'])
+def obtener_cursos():
+
+    cursos = []
+    carnet = request.json['carnet']
+    cursor.execute("SELECT * FROM cursosAprobados WHERE Carnet = " + f"'{carnet}';")
+    
+    for curso in cursor:
+        objeto = {
+            'Nombre': curso[2]
+        }
+        cursos.append(objeto)
+    return(jsonify(cursos))
+    
+@app.route('/datos/<string:carnet>', methods = ['GET'])
+def obtener_datos(carnet):
+    if request.method == 'GET':
+        registro = carnet
+        cursor.execute("SELECT * FROM Usuarios WHERE Carnet = " + f"'{registro}';")
+        for usuario in cursor:
+            if (usuario[2] == registro):
+                objeto = {
+                    'nombre': usuario[0],
+                    'apellido': usuario[1],
+                    'carnet': usuario[2],
+                    'correo': usuario[4],
+                    'contraseña': usuario[3],
+                    'mensaje': 'datos cargados con éxito'
+                }
+                return(jsonify(objeto))
+            else:
+                objeto = {
+                    'mensaje':'el nombre de usuario no existe'
+                }
+                return(jsonify(objeto))
+        objeto = {
+            'mensaje':'el nombre de usuario es incorrecto o no existe'
+        }
+        return(jsonify(objeto))
+
+@app.route('/modificar_datos', methods = ['POST'])
+def modificar_datos():
+    nombre = request.json['nombre']
+    apellido = request.json['apellido']
+    correo = request.json['correo']
+    carnet = request.json['registro']
+    if request.method == 'POST':
+        try:
+            cursor.execute("UPDATE Usuarios SET Nombre = " + f"'{nombre}'" + ", " + "Apellido = " + f"'{apellido}'" + ", " + "Correo = " + f"'{correo}'" + " WHERE Carnet = " + f"'{carnet}';")
+            conn.commit()
+            objeto = {
+                'mensaje':'Datos de usuario editados exitosamente'
+            }
+            return(jsonify(objeto))
+        except:
+            objeto = {
+                'mensaje':'Datos de usuario editados exitosamente'
+            }
+            return(jsonify(objeto))
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 3000, debug = True)
